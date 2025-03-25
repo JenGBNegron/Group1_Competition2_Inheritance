23d21de7955b3684a2078a84110fd7fe3da24013
subhan-faisal <subhanfaisal1@gmail.com>
2025-03-25 10:51:53 -0400

    Implemented Library Management System

---
+++ USF_Library_Management_System/Classes/Library.cs
@@ -0,0 +1,28 @@
+namespace Classes
+{
+    public class Library
+{
+    public List<Book> Books { get; set; } = new List<Book>();
+    public List<Person> Patrons { get; set; } = new List<Person>();
+
+    public void DisplayBooks()
+    {
+        Console.WriteLine("Books in Library:");
+        foreach (var book in Books)
+        {
+            Console.WriteLine($"Title: {book.Title}, Author: {book.Author}, Available Copies: {book.AvailableCopies}");
+        }
+    }
+
+    public void DisplayPatrons()
+    {
+        Console.WriteLine("Patrons in Library:");
+        foreach (var patron in Patrons)
+        {
+            Console.WriteLine($"Name: {patron.Name}, ID: {patron.ID}");
+        }
+    }
+}
+
+}
+
