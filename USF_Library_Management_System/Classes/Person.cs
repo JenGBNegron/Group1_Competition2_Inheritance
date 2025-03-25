4848252b9703b2e38fcf46347a9995e21c72f431
Jennifer Negron <jgb1@usf.edu>
2025-03-25 10:18:56 -0400

    Pushing classes folder and person class.

---
+++ USF_Library_Management_System/Classes/Person.cs
@@ -0,0 +1,46 @@
+using System.Diagnostics.Tracing;
+
+namespace Classes
+{
+   public class Person
+{
+    // Backing fields
+    private string name;
+    private string email;
+    private int id;
+
+    // Properties with getters and setters
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public string Email
+    {
+        get { return email; }
+        set { email = value; }
+    }
+
+    public int ID
+    {
+        get { return id; }
+        set { id = value; }
+    }
+
+    // Constructor
+    public Person(string name, string email, int id)
+    {
+        this.name = name;
+        this.email = email;
+        this.id = id;
+    }
+
+    // Optional: Override ToString for easy display
+    public override string ToString()
+    {
+        return $"Name: {Name}, Email: {Email}";
+
+    }
+}
+}
--- USF_Library_Management_System/obj/Debug/net9.0/USF_Library_Management_System.AssemblyInfo.cs
+++ USF_Library_Management_System/obj/Debug/net9.0/USF_Library_Management_System.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("USF_Library_Management_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f25081a3b4fc8e3c508a7932c9e3ec4ef2838b8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2cf43ee2d9fb76e60b1d3bc76d5a1b4b47cdab8")]
 [assembly: System.Reflection.AssemblyProductAttribute("USF_Library_Management_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("USF_Library_Management_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
